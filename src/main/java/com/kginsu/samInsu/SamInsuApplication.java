3e2621465d71b846c362391014e41975690f438f
Karthikeyan K <karthikeyan.k@KGFSL.COM>
2017-10-05 16:37:36 +0530

    Initial Commit

---
+++ src/main/java/com/kginsu/samInsu/SamInsuApplication.java
@@ -0,0 +1,12 @@
+package com.kginsu.samInsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamInsuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamInsuApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kginsu/samInsu/SamInsuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kginsu.samInsu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SamInsuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
